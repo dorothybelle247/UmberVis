5a17f539ac5338fbe0e01634262ac56b6b70adad
dorothybelle247 <dorothybelle247@gmail.com>
2021-03-29 18:11:43 -0700

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
 import React from "react";
 import "./style.css";
+import {arc} from "d3";
 
-const width = 960;
-const height = 500;
+console.log(arc)
+
+const width = 920;
+const height = 460;
 const centerX = width / 2;
-const centerY = height/2;
-const strokeWidth = 30;
+const centerY = height / 2;
+const strokeWidth = 15;
+const eyeOffsetX = 90;
+const eyeOffsetY = 110;
+const eyeRadius = 25;
+const mouthWidth = 15;
+const mouthRadius = 130;
+// Method chaining - set multiple chain
 
-export default function App() {
-  return (
+
+const noseArc = arc()
+    .innerRadius(0)
+    .outerRadius(40)
+    .startAngle(0)
+    .endAngle(Math.PI / 2);
+
+const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI /2)
+    .endAngle(Math.PI * 3/2);
+  
+const App = () => 
+ (
     <div>
-       <svg width={width} height={height}>
-        <circle cx={centerX} cy={centerY} r="200" fill="pink" stroke="black" stroke-width={strokeWidth}></circle>
+      <svg width={width} height={height}>
+       <g transform={`translate(${centerX}, ${centerY}) rotate(9)`}>
+        <circle
+          r={centerY - strokeWidth / 2}
+          fill="pink"
+          stroke="black"
+          stroke-width={strokeWidth}
+        />
 
-        <circle cx="330" cy="180" r={centerY -strokeWidth / 2} fill="black" stroke="black" stroke-width="8"></circle>
+        <circle
+          cx={-eyeOffsetX}
+          cy={-eyeOffsetY}
+          r={eyeRadius}
+          // fill="black"
+          // stroke="black"
+          // stroke-width="8"
+        />
 
-        <circle cx="480" cy="250" r="200" fill="pink" stroke="black" stroke-width="8"></circle>
-    </svg>
+        <circle
+          cx={eyeOffsetX}
+          cy={- eyeOffsetY}
+          r={eyeRadius}
+          // fill="black"
+          // stroke="black"
+          // stroke-width="8"
+        />
+        <path d={noseArc()}/>
+        <path d={mouthArc()}/>
+        </g>
+      </svg>
     </div>
   );
-}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {arc} from "d3";
 
 import App from "./App";
 
